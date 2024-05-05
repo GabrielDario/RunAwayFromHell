219844b5d3105f3c3ca65cf736b085a99c3b020d
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-05-04 23:39:54 -0300

    Update README.md: add details about gameplay and 3rd floor features
    * Added section about how the ranking system works and will be implemented later
    * Expanded on the "JOGAR" section to include more details

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Pressable ,Image} from 'react-native';
 
 export default function App() {
+  const onPressFunction = (req, res) => {
+    console.log('Teste')
+  }
   return (
     <View style={styles.container}>
-      <Text>Run away from hell</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/HomeScreen.png')} resizeMode="cover" style={styles.image}>
+        <Image
+          style={styles.logoImg}
+          source={require('./assets/logo.png')}
+        />
+        <View style={styles.menus}>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Novo Jogo</Text>
+          </Pressable>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Apoiar</Text>
+          </Pressable>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Ranking</Text>
+          </Pressable>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Opções</Text>
+          </Pressable>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Créditos</Text>
+          </Pressable>
+          <Pressable onPress={onPressFunction} style={styles.btn}>
+            <Text style={styles.textBtn}>Sair</Text>
+          </Pressable>
+        </View>
+
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +40,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  image: {
+    flex: 1,
+  },
+  logoImg: {
+    top:50,
+
+  },
+  menus: {
+    top: 100
+  },
+  btn: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 20
+  },
+  textBtn: {
+    lineHeight: 60,
+    fontWeight: 'bold',
+    color: 'yellow',
+    fontSize: 30,
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: 'white',
+  }
 });
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # RunAwayFromHell
+
+Para rodar celular - npx expo
+
 Feito em React Native - point click aonde o personagem começa no primeiro andar e o objetivo é chegar no 10º andar
 
  - Menu terá
@@ -14,9 +17,17 @@ Feito em React Native - point click aonde o personagem começa no primeiro andar
 ----------------------------------------------------
    RANKING
    Pegara no banco de dados os 10 melhores da pontuação
-   - Como funciona a pontuação
+   - Como funciona a pontuação - Fazer mais para frente
 ----------------------------------------------------
   JOGAR   
  - Você esta preso em um lugar e tem quatro portas,norte,sul,leste e oeste
+ - Você tem que achar a porta correta para subir de andar! começando do 1º
  - Nos primeiro andares (1º e 2ª) 
+ - A cada andar, a dificuldade aumentará
+ - No 3º andar ocorre as seguintes mudanças: 
+ - Você possui uma vida de 100 de HP ;
+ - Uma nova ação clicando na lupa aonde você vasculha coisas que podem conter : DICAS, OBJETOS e BANDAID
+ - DICAS você encontrará pistas de como sair do andar que você está;
+ - OBJETOS será necessário para certas ações para subir de andar;
+ - BANDID para aumentar o seu HP EM 20;
 ----------------------------------------------------
